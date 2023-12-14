2cccb173527bcdc31137af1f28e328b89fc0c6b5
Ahmed JABRI <ahmed.jabri10@gmail.com>
2023-12-14 19:05:01 +0100

    my changes1

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ import statsmodels
 from statsmodels.tsa.arima.model import sarimax
 
 
-st.write(''' #Pridection of electrical consumption in France''')
+st.write(''' #Prediction of electrical consumption in France''')
 st.sidebar.header("Inputs")
-         
+def user_input(): 
+    Month=st.sidebar.slider('Month', 1, 12, 6)
+    Year=st.sidebar.slider('Year', 2023, 2030, 2024)
+    
+Month=input('Month')
+Year=input('Year')  
+List=[Month, Year, '01']
+
+ 
+#Prediction_date=pd.DataFrame(data, index=[0])
+ #   return Prediction_date
+#df=user_input()     ''''   
